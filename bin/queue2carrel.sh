fbb5e9e7cdfab9c6e80474642f9880fb58ffd183
Eric Lease Morgan <eric_morgan@infomotions.com>
2020-11-29 01:35:30 +0000

    Added Biorxiv

--- bin/queue2carrel.sh
+++ bin/queue2carrel.sh
@@ -8,16 +8,18 @@
 # August    1, 2018 - first investigations and in Lake Geneva after HathiTrust workshop in Milwaukee; "Happy birthday, Barb!"
 # August    2, 2018 - back at home and getting closer; works, but only one at a time
 # November 27, 2020 - refining to use TSV input; during Thanksgiving holiday in Lancaster and during a pandemic; go figure
+# November 28, 2020 - added url2carrel and biorxiv2carrel
 
 
 # configure
+BACKLOG='/data-disk/reader-compute/reader-classic/queue/backlog'
+BIOARXIV2CARREL='bioarxiv2carrel.sh'
 CARRELS="$READERCLASSIC_HOME/carrels"
 FILE2CARREL='file2carrel.sh'
+GUTENBERGQUEUE2ZIP='gutenbergqueue2zip.pl'
+SEARCH='search.pl'
 URL2CARREL='url2carrel.sh'
 URLS2CARREL='urls2carrel.sh'
-ZOTERO2CARREL='zotero2carrel.sh'
-SEARCH='search.pl'
-GUTENBERGQUEUE2ZIP='gutenbergqueue2zip.pl'
 ZIP2CARREL='zip2carrel.sh'
 
 # get the input
@@ -52,7 +54,7 @@ cat $TSV | while read TYPE SHORTNAME DATE TIME EMAIL CONTENT; do
 	# for reasons of provenance, copy the queue file to the carrel
 	cp $TSV "$CARRELS/$SHORTNAME/provenance.tsv"
 
-	# check for valid/known processes
+	# check for valid/known processes; gutenberg
 	if [[ $TYPE = "gutenberg" ]]; then
 		
 		# create the zip file and its metadata
@@ -64,7 +66,24 @@ cat $TSV | while read TYPE SHORTNAME DATE TIME EMAIL CONTENT; do
 		cd "$CARRELS/$SHORTNAME"
 		$ZIP2CARREL ./input-file.zip 1>standard-output.txt 2>standard-error.txt &
 
-	# unknown process; update database
+	# url2carrel
+	elif [[ $TYPE = "url2carrel" ]]; then
+	
+		# make sane and do the work
+		echo "Making sane and doing the work ($URL2CARREL)" >&2
+		cd "$CARRELS/$SHORTNAME"
+		$URL2CARREL "$CONTENT" 1>standard-output.txt 2>standard-error.txt &
+	
+	# biorxiv
+	elif [[ $TYPE = "biorxiv" ]]; then
+	
+		# make sane and do the work
+		echo "Making sane and doing the work ($BIOARXIV2CARREL)" >&2
+		cd "$CARRELS/$SHORTNAME"
+		mv $BACKLOG/$CONTENT ./biorxiv.xml
+		$BIOARXIV2CARREL ./biorxiv.xml 1>standard-output.txt 2>standard-error.txt &
+	
+	# unknown process
 	else 
 
 		echo "Error: Unknown type ($TYPE). Call Eric.\n" >&2
